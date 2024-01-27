5a596252398510d9c664c30c0baa89c794e48221
Abhinav Ratan <abhinavratan123@gmail.com>
2024-01-28 00:00:48 +0530

    Product Page

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
 import EcommerceHomePage from './Homepage';
+import ProductPage from './productpage';
 import "./App.css";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         {/* Route for Sign Up */}
         <Route path="/signup" element={<SignUp />} />
         <Route path="/home" element={<EcommerceHomePage />} />
+        <Route path="/productpage" element={<ProductPage />} />
       </Routes>
     </Router>
     </>
